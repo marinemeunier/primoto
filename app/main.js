c0eeff282f34d9c4d96c1e90ed7c36edb4b3460e
OmairAI <omairarshadiqbal@gmail.com>
2018-12-17 23:46:25 +0100

    Change project architecture to Angular standard architecture

--- app/main.js
+++
@@ -1,8 +0,0 @@
-(function(app) {
-
-  'use strict';
-  document.addEventListener('DOMContentLoaded', function() {
-    ng.platformBrowserDynamic.bootstrap(app.AppComponent);
-  });
-  
-})(window.app || (window.app = {}));
